3ff82d19dc98eb3a44387c35a6d79a97f01a0a68
sushmasingh2000 <sushmasingh16166@gmail.com>
2024-12-09 17:49:10 +0530

    jhghj

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,29 +4,20 @@ const { queryDb } = require("../helper/adminHelper");
 exports.Registration = async (req, res) => {
     const { username, email, mobile_no, set_password, confirm_password } = req.body;
     if (!username || !email || !mobile_no || !set_password || !confirm_password) {
-        return res.status(400).json({ msg: 'All field is required' });
+        return res.status(400).json({ msg: 'All fields are required' });
     }
-    if (set_password !== confirm_password)
-        return res.status(201).json({
-            msg: `Password and confirm password doesn't match.`,
-        });
     try {
-        const checkQuery = 'SELECT * FROM Registration WHERE email = ? ';
-        const duplicateUser = await queryDb(checkQuery, [email]);
-        if (duplicateUser.length > 0) {
-            return res.status(400).json({ msg: 'Email already registered.' });
-        }
-
-        const insertQuery = 'INSERT INTO Registration (username, email, mobile_no, set_password ,confirm_password ) VALUES (?, ?, ?, ?, ?)';
-        const result = await queryDb(insertQuery, [username, email, mobile_no, set_password, confirm_password]);
-        const userId = result.insertId;
-
+        // Call the stored procedure to insert data
+        const procedureQuery = 'CALL registration(?, ?, ?, ?, ?)';
+        const result = await queryDb(procedureQuery, [username, email, mobile_no, set_password, confirm_password]);
+        const userId = result.insertId; 
         return res.status(200).json({ msg: "Registered successfully", userId: userId, data: result });
     } catch (e) {
         console.error(e);
-        return res.status(500).json({ msg: "Something went wrong in the API call." });
+        return res.status(500).json({ msg: e.sqlMessage || "Something went wrong in the API call." });
     }
 };
+
 
 exports.Login = async (req, res) => {
     const { email, password } = req.body;
